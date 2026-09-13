1f19f615cfb449b486ef05a8eada74ad43f8f106
Kai Leonov <cpt.leo@proton.me>
2026-09-13 12:30:55 +0200

    new python file simulating zabbix ping

---
+++ ping.py
@@ -0,0 +1,27 @@
+import random
+
+servers = {
+    "DB server":"192.168.1.5",
+    "Web server":"192.168.1.10",
+    "Wazuh server":"192.168.1.15"
+}
+    
+def generate_ping():
+    ping_latency = random.randint(10, 500)
+    return ping_latency    
+    
+def check_server(server, ip):
+
+    current_ping = generate_ping()
+    print(f"Starting checks: {server} {ip}")
+    print(f"Ping on server {server} is {current_ping} ms")
+    return current_ping
+    
+
+for server, ip in servers.items():
+    latency = check_server(server, ip)
+    
+    if latency > 300:
+        print("Warning! Ping is too high!")
+    else:
+        print("Status: Everything is fine.")
